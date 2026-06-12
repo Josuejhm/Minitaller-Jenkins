bc07b6dc6b5575fbe97891154516b8092eb320f2
Josuehmj <josueahm16gmail.com>
2026-06-12 13:09:11 -0600

    Tutorial: corregir test de minas

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -56,7 +56,7 @@ class TestTableroFacil(unittest.TestCase):
     def test_cantidad_de_minas_en_dificultad_facil(self):
         board = generate_board(8, 8, 10, (0, 0), rng=random.Random(42))
         mine_count = sum(cell == MINE for row in board for cell in row)
-        self.assertEqual(mine_count, 5)  # <-- error itencional
+        self.assertEqual(mine_count, 10)  # <-- error itencional
 
 if __name__ == "__main__":
     unittest.main()
